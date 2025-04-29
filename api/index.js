31add046b7ebe3d6ecebd6d1c64fd8fcc9e86d34
Matheus Oliveira <59532044+radonix@users.noreply.github.com>
2025-04-29 22:19:41 +0000

    feat: add db connection and first db model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import db from "./database/configdb.js"
+import User from "./models/User.js"
 dotenv.config();
+
+db.connect();
 
 const app = express();
 app.use(express.json());
